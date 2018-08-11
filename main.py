b48ee99226363f1b151ffe5b26fc397f7fcfb36e
christopherhui <christophergkhui@gmail.com>
2018-08-10 18:52:18 -0700

    drawing triangle init. to spaceship

--- main.py
+++ main.py
@@ -17,10 +17,33 @@ FPS = 60
 pygame.display.set_caption('Spaceship Game')
 
 background = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+background.fill(WHITE)
 
 # Init. Spaceship
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
-        pygame.Sprite.sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
 
-        self.image
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+
+        pygame.draw.polygon(self.image, color, [[WIDTH / 2, HEIGHT - height],[WIDTH / 2 - width / 2, HEIGHT],[WIDTH / 2 + width / 2, HEIGHT]])
+
+    def drawShip(self):
+        ship_list = pygame.sprite.Group()
+        ship_list.add(self)
+        ship_list.draw(background)
+
+def main():
+    a = Spaceship(BLUE, 100, 100)
+    a.drawShip()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        pygame.display.update()
+
+main()
